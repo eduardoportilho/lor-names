df64481978e4177ae6f5d22596c1b36bf254080a
Eduardo Pilla Portilho <eduardoportilho@spotify.com>
2017-01-26 09:27:24 +0100

    9.2 codecov ok

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
-import uniqueRandomArray from 'unique-random-array';
-import lorNames from './lor-names.json';
+import uniqueRandomArray from 'unique-random-array'
+const lotrNames = require('./lotr-names.json')
 
-const getRandomItem = uniqueRandomArray(lorNames);
-
-module.exports = {
-  all: lorNames,
-  random: random
-};
-
-function random(number) {
-  if (number === undefined) {
-    return getRandomItem();
-  } else {
-    const randomItems = [];
-    for (let i = 0; i < number; i++) {
-      randomItems.push(getRandomItem());
-    }
-    return randomItems;
-  }
+const mainExport = {
+  all: lotrNames,
+  random: uniqueRandomArray(lotrNames),
 }
+
+export default mainExport
+module.exports = mainExport // for CommonJS compatibility
--- src/index.test.js
+++ src/index.test.js
@@ -1,35 +1,20 @@
-import {expect} from 'chai';
-import lor from './index';
+import {expect} from 'chai'
+import lotrNames from '.'
 
-describe('lor-names', function() {
-  describe('all', function() {
-    it('should be an array of strings', function() {
-      expect(lor.all).to.satisfy(isArrayOfStrings);
+describe('lotr-names', () => {
+  it('should have a list of all available names', () => {
+    expect(lotrNames.all).to.satisfy(isArrayOfStrings)
+  })
 
-      function isArrayOfStrings(array) {
-        return array.every(function(item) {
-          return typeof item === 'string';
-        });
-      }
-    });
+  it('should allow me to get a random name from the list', () => {
+    expect(lotrNames.random()).to.satisfy(isIncludedIn(lotrNames.all))
+  })
+})
 
-    it('should contain `Luke Skywalker`', function() {
-      expect(lor.all).to.include('Luke Skywalker');
-    });
-  });
+function isArrayOfStrings(array) {
+  return array.every(i => typeof i === 'string')
+}
 
-  describe('random', function() {
-    it('should return a random item from the lor.all', function() {
-      var randomItem = lor.random();
-      expect(lor.all).to.include(randomItem);
-    });
-
-    it('should return an array of random items if passed a number', function() {
-      var randomItems = lor.random(3);
-      expect(randomItems).to.have.length(3);
-      randomItems.forEach(function(item) {
-        expect(lor.all).to.include(item);
-      });
-    });
-  });
-});
+function isIncludedIn(array) {
+  return item => array.includes(item)
+}
